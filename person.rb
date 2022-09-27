a99a86fa535bfe818c37b687edeef336bbb83ec3
wayungi <fwayungi@gmail.com>
2022-09-27 23:14:14 +0300

    Add can_use_service, of_age methods

--- person.rb
+++ person.rb
@@ -8,4 +8,16 @@ class Person
 
   attr_accessor :name, :age
   attr_reader :id
+
+  def can_use_services?
+    of_age || @parent_permission
+  end
+
+  private
+
+  def of_age
+    return true if @age >= 18
+
+    false
+  end
 end
