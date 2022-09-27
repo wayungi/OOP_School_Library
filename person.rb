9a919a2d5625a1100b3729f6cbfb723807c6f376
wayungi <fwayungi@gmail.com>
2022-09-27 22:55:08 +0300

    Add getters

--- person.rb
+++ person.rb
@@ -5,4 +5,17 @@ class Person
     @age = age
     @parent_permission = parent_permission
   end
+
+  # define getters
+  def id
+    @id
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
 end
