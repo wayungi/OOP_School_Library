e42aac2efde32cb02bf28a94fe50ed2e60444f19
wayungi <fwayungi@gmail.com>
2022-09-28 22:44:55 +0300

    Fix linter errors

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,11 +1,9 @@
 class TrimmerDecorator < BaseDecorator
-    def initialize(nameable)
-        @nameable = nameable
-    end
+  def initialize(nameable)
+    @nameable = nameable
+  end
 
-    def correct_name
-        @nameable.correct_name[0...10]
-    end
-
+  def correct_name
+    @nameable.correct_name[0...10]
+  end
 end
-
