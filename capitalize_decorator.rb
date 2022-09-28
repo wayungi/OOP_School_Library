6a1cb6bdafb7529db584f5cc0164bed02594f60c
wayungi <fwayungi@gmail.com>
2022-09-28 21:54:43 +0300

    Disable Lint?MissingSuperCall

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,10 +1,10 @@
-class BaseDecorator < Nameable
+class CapitalizeDecorator < BaseDecorator
   def initialize(nameable)
     super()
     @nameable = nameable
   end
 
   def correct_name
-    @nameable.correct_name
+    @nameable.correct_name.capitalize
   end
 end
