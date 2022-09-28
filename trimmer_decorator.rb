fd51fd50a57560ea6536a9d5b3ec47ba2264b263
wayungi <fwayungi@gmail.com>
2022-09-28 22:22:34 +0300

    Add trimmer_decorator

---
+++ trimmer_decorator.rb
@@ -0,0 +1,11 @@
+class TrimmerDecorator < BaseDecorator
+    def initialize(nameable)
+        @nameable = nameable
+    end
+
+    def correct_name
+        @nameable.correct_name[0...10]
+    end
+
+end
+
