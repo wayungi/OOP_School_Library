ef7d45179c9cb8a5bfcc0ac4aa5718fe9d2ceeec
wayungi <fwayungi@gmail.com>
2022-09-30 00:12:01 +0300

    Implement many-to-many relationship

--- rental.rb
+++ rental.rb
@@ -1,7 +1,15 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :person, :book
 
-  def initialize(date)
+  def initialize(date, person, book)
     @date = date
+
+    @person = person
+    # Add the rental record to the person object
+    person.rentals << self
+
+    @book = book
+    # add the rental record to the book object
+    book.rentals << self
   end
 end
