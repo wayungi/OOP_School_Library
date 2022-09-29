0ecc1866ae0408f2861cbe9cb4a697e154c4cbe2
wayungi <fwayungi@gmail.com>
2022-09-30 00:01:34 +0300

    Add attr_rentals

--- book.rb
+++ book.rb
@@ -1,8 +1,10 @@
 class Book
   attr_accessor :author, :title
+  attr_reader :rentals
 
   def initialize(author, title)
     @author = author
     @title = title
+    @rentals = []
   end
 end
--- rental.rb
+++ rental.rb
@@ -1,7 +1,7 @@
 class Rental
   attr_accessor :date
-  
+
   def initialize(date)
-    @date = date 
+    @date = date
   end
 end
