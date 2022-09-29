34cb09e0aeb2c73c8d9540c8a6d77076f40cf4f2
wayungi <fwayungi@gmail.com>
2022-09-29 23:07:55 +0300

    Add many side of student-classroom relatioship

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,14 @@
 class Classroom
     attr_accessor :label
+    attr_reader :students
 
     def initialize(label)
         @label = label
+        @students = []
+    end
+
+    # Add a student to the classroom
+    def add_student(student)
+        students.push(student)
     end
 end
